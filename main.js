6206a474f2b8cd98112fe18a352bc2aae274fa5c
titus <lesinko2@gmail.com>
2022-06-29 15:47:58 +0300

    updated css desktop component

---
+++ main.js
@@ -0,0 +1,8 @@
+// // get selected rate from localStorage
+// var rating = localStorage.getItem('rate-number');
+
+// // feedback response
+// let rated-response = 'You selected $(rating) out of 5'
+
+// // Append response text
+// document.getElementById.('response').innerText = rated-response;
