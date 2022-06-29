57d0a558918c58c5becf7459e9cf9570d1256628
titus <lesinko2@gmail.com>
2022-06-29 20:57:50 +0300

    component design updates
    added js

--- main.js
+++
@@ -1,8 +0,0 @@
-// // get selected rate from localStorage
-// var rating = localStorage.getItem('rate-number');
-
-// // feedback response
-// let rated-response = 'You selected $(rating) out of 5'
-
-// // Append response text
-// document.getElementById.('response').innerText = rated-response;
